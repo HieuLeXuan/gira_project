d4aaff8a9a6f88c22bdf18df68f58b9567a40914
Le Xuan Hieu <hieulexuan629@gmail.com>
2022-09-17 15:48:47 +0700

    setup code base

---
+++ src/main/java/cybersoft/javabackend/java18/GiraProjectApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/javabackend/java18/GiraProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
