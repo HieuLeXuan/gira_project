c626ee95e20e4732de830588b12a5b7f10b0a67c
Le Xuan Hieu <hieulexuan629@gmail.com>
2022-09-18 15:44:06 +0700

    feat: add baseEntity with jpa auditor

---
+++ src/main/java/cybersoft/javabackend/java18/common/util/DateTimeUtils.java
@@ -0,0 +1,17 @@
+package cybersoft.javabackend.java18.common.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @UtilityClass chuc nang khong tao instance cua lop nay
+ */
+@UtilityClass
+public class DateTimeUtils {
+
+    /**
+     * dinh dang don gian cho lam do an, demo
+      */
+
+    public static final String DATETIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
+    
+}
